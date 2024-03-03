2376375b651b7a91ac9a3e812ab37825c086d8c1
elifsuperisan <161431452+elifsuperisan@users.noreply.github.com>
2024-03-03 17:11:29 +0300

    Add files via upload

---
+++ src/timeArrayTest.java
@@ -0,0 +1,16 @@
+public class timeArrayTest {
+    public static void main(String[] args) {
+
+        int[] timePeriods = {341, 273, 278, 329, 445, 402, 388, 270, 243, 334, 412, 393, 299, 343, 317, 265};
+        String[] names = {"Kadir", "Gökhan", "Hakan", "Suzan", "Pınar", "Mehmet", "Ali", "Emel", "Fırat", "James", "Jale", "Ersin", "Deniz", "Gözde", "Anıl", "Burak" };
+
+        timeArray myMaraton = new timeArray(timePeriods);
+
+        System.out.println("Birinci: " + names[myMaraton.first()] + " " + timePeriods[myMaraton.first()] + "'");
+        System.out.println("İkinci: " + names[myMaraton.second()] + " " + timePeriods[myMaraton.second()] + "'");
+        System.out.println("Üçüncü: " + names[myMaraton.third()] + " " + timePeriods[myMaraton.third()] + "'");
+
+        myMaraton.group();
+
+    }
+}
